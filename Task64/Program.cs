7238a55679f6ebdb1aa5d8eeb33f79af6a7e9892
SergyVR <rsv1177@gmail.com>
2022-11-11 23:24:38 +0300

    first commit

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число N"); 
+int number = int.Parse(Console.ReadLine()); 
+ 
+void CountNumbers (int number)
+{
+   
+    if (number == 0) return;
+    Console.Write($"{number} ,");
+    CountNumbers(number - 1);
+}
+
+CountNumbers(number);
---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M от которого считать"); 
+int m = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите число N до которого считать"); 
+int n = int.Parse(Console.ReadLine()); 
+
+
+void GapNumberSum (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N (включительно) = {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    GapNumberSum(m, n, sum);
+}
+
+GapNumberSum(m, n, 0);
